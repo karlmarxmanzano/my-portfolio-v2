bf5ef4d21ff8c476993e87857c08d73eb9380052
Karl Marx Manzano <karlmarxmanzano@gmail.com>
2023-07-19 12:36:30 +0800

    Add tailwindcss

---
+++ pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/events">Events</NuxtLink>
+          </li>
+        </ul>
+      </header>
+    Welcome to Index
+  </div>
+</template>
