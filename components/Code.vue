e19f1cbe9c012d76873c3d7b72dcf1e8f04de28f
Karl Marx Manzano <karlmarxmanzano@gmail.com>
2025-03-11 19:40:32 +0800

    chore: added projects

--- components/Code.vue
+++ components/Code.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <code class="p-0.5 dark:bg-gray-700 text-xs font-semibold rounded">
+  <code
+    class="p-0.5 bg-slate-200 dark:bg-gray-700 text-xs font-semibold rounded"
+  >
     <slot />
   </code>
 </template>
